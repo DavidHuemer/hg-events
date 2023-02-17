84745ae8bf5e72d7edb3538f5eae3742c599e305
David Huemer <92david.huemer@gmail.com>
2023-02-17 16:01:38 +0100

    Added basic empty-container

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,12 +1,18 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { EmptyContainerComponent } from './components/empty-container/empty-container.component';
 
 
 
 @NgModule({
-  declarations: [],
-  imports: [
-    CommonModule
-  ]
+    declarations: [
+        EmptyContainerComponent
+    ],
+    exports: [
+        EmptyContainerComponent
+    ],
+    imports: [
+        CommonModule
+    ]
 })
 export class SharedModule { }
