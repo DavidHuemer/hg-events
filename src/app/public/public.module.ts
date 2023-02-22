50fa9e138a923f9306fbec5efca00c68db9ead1b
David Huemer <92david.huemer@gmail.com>
2023-02-22 23:00:19 +0100

    Updated footer style

--- src/app/public/public.module.ts
+++ src/app/public/public.module.ts
@@ -1,15 +1,16 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import {NgModule} from '@angular/core';
+import {CommonModule} from '@angular/common';
 
-import { PublicRoutingModule } from './public-routing.module';
-import { HeaderComponent } from './components/header/header.component';
-import { FooterComponent } from './components/footer/footer.component';
+import {PublicRoutingModule} from './public-routing.module';
+import {HeaderComponent} from './components/header/header.component';
+import {FooterComponent} from './components/footer/footer.component';
 import {MatIconModule} from "@angular/material/icon";
-import { PublicPageComponent } from './pages/public-page/public-page.component';
+import {PublicPageComponent} from './pages/public-page/public-page.component';
 import {MatCardModule} from "@angular/material/card";
 import {MatDividerModule} from "@angular/material/divider";
 import {MatButtonModule} from "@angular/material/button";
-import { EquipmentsPageComponent } from './pages/equipments-page/equipments-page.component';
+import {EquipmentsPageComponent} from './pages/equipments-page/equipments-page.component';
+import {SocialMediaButtonComponent} from './components/social-media-button/social-media-button.component';
 
 
 @NgModule({
@@ -17,7 +18,8 @@ import { EquipmentsPageComponent } from './pages/equipments-page/equipments-page
     HeaderComponent,
     FooterComponent,
     PublicPageComponent,
-    EquipmentsPageComponent
+    EquipmentsPageComponent,
+    SocialMediaButtonComponent
   ],
   imports: [
     CommonModule,
