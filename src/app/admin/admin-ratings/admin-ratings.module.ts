04b32027829aaa96c13a788a1ece8f60b261f3f3
David Huemer <92david.huemer@gmail.com>
2023-02-22 11:46:25 +0100

    Added admin-ratings module

---
+++ src/app/admin/admin-ratings/admin-ratings.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { AdminRatingsRoutingModule } from './admin-ratings-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    AdminRatingsRoutingModule
+  ]
+})
+export class AdminRatingsModule { }
