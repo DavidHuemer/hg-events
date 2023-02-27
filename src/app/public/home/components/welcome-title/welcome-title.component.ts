7800d55c3d9db7030b3c259f6932dd9b10094d09
David Huemer <92david.huemer@gmail.com>
2023-02-27 14:00:27 +0100

    Added welcome-title component

---
+++ src/app/public/home/components/welcome-title/welcome-title.component.ts
@@ -0,0 +1,16 @@
+import {Component, OnInit} from '@angular/core';
+
+@Component({
+  selector: 'app-welcome-title',
+  templateUrl: './welcome-title.component.html',
+  styleUrls: ['./welcome-title.component.scss']
+})
+export class WelcomeTitleComponent implements OnInit {
+
+  constructor() {
+  }
+
+  ngOnInit(): void {
+  }
+
+}
