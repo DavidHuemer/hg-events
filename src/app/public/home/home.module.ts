83f051679b3c92a6a98ed81001ce45f384527a3b
David Huemer <92david.huemer@gmail.com>
2023-02-27 14:12:47 +0100

    Updated welcome-area

--- src/app/public/home/home.module.ts
+++ src/app/public/home/home.module.ts
@@ -1,13 +1,14 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import {NgModule} from '@angular/core';
+import {CommonModule} from '@angular/common';
 
-import { HomeRoutingModule } from './home-routing.module';
-import { HomePageComponent } from './home-page/home-page.component';
-import { WelcomeAreaComponent } from './components/welcome-area/welcome-area.component';
-import { HomeOverviewAreaComponent } from './components/home-overview-area/home-overview-area.component';
+import {HomeRoutingModule} from './home-routing.module';
+import {HomePageComponent} from './home-page/home-page.component';
+import {WelcomeAreaComponent} from './components/welcome-area/welcome-area.component';
+import {HomeOverviewAreaComponent} from './components/home-overview-area/home-overview-area.component';
 import {MatButtonModule} from "@angular/material/button";
 import {MatCardModule} from "@angular/material/card";
-import { HomeOverviewItemComponent } from './components/home-overview-item/home-overview-item.component';
+import {HomeOverviewItemComponent} from './components/home-overview-item/home-overview-item.component';
+import {WelcomeTitleComponent} from './components/welcome-title/welcome-title.component';
 
 
 @NgModule({
@@ -15,7 +16,8 @@ import { HomeOverviewItemComponent } from './components/home-overview-item/home-
     HomePageComponent,
     WelcomeAreaComponent,
     HomeOverviewAreaComponent,
-    HomeOverviewItemComponent
+    HomeOverviewItemComponent,
+    WelcomeTitleComponent
   ],
   imports: [
     CommonModule,
