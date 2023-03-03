0f55d9400b3724ce2035edf68b3bc566b31ccd47
David Huemer <92david.huemer@gmail.com>
2023-03-03 14:52:56 +0100

    Added contact module

--- src/app/public/public-routing.module.ts
+++ src/app/public/public-routing.module.ts
@@ -8,7 +8,8 @@ const routes: Routes = [
       {path: '', loadChildren: () => import('./home/home.module').then(m => m.HomeModule)},
       {path: 'equipments', loadChildren: () => import('./equipments/equipments.module').then(m => m.EquipmentsModule)},
       {path: 'ratings', loadChildren: () => import('./ratings/ratings.module').then(m => m.RatingsModule)},
-      {path: 'requests', loadChildren: () => import('./requests/requests.module').then(m => m.RequestsModule)}
+      {path: 'requests', loadChildren: () => import('./requests/requests.module').then(m => m.RequestsModule)},
+      {path: 'contact', loadChildren: () => import('./contact/contact.module').then(m => m.ContactModule)}
     ]
   }
 ];
