46c4cafd114a67e0d47a60365bf0252ca0cb2ae5
David Huemer <92david.huemer@gmail.com>
2023-03-06 14:03:48 +0100

    The menu now closes when the url changes

--- src/app/public/components/header/header/header.component.ts
+++ src/app/public/components/header/header/header.component.ts
@@ -1,4 +1,5 @@
 import {Component, OnInit} from '@angular/core';
+import {Router} from "@angular/router";
 
 @Component({
   selector: 'app-header',
@@ -7,14 +8,18 @@ import {Component, OnInit} from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
 
-  constructor() { }
+  show: boolean = false;
 
-  show : boolean = false;
+  constructor(private router: Router) {
+    router.events.subscribe(() => {
+      this.show = false;
+    });
+  }
 
   ngOnInit(): void {
   }
 
-  toggle(){
+  toggle() {
     this.show = !this.show;
   }
 
