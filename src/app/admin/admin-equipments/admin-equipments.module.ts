bda004f12a8bf74fa53b637e25e64aef54675534
David Huemer <92david.huemer@gmail.com>
2023-03-21 12:49:32 +0100

    Updated basic edit equipment layout

--- src/app/admin/admin-equipments/admin-equipments.module.ts
+++ src/app/admin/admin-equipments/admin-equipments.module.ts
@@ -7,6 +7,11 @@ import {AdminRatingsModule} from "../admin-ratings/admin-ratings.module";
 import {AdminModule} from "../admin.module";
 import {AdminEquipmentRowComponent} from './components/admin-equipment-row/admin-equipment-row.component';
 import {EditEquipmentPageComponent} from './pages/edit-equipment-page/edit-equipment-page.component';
+import {MatButtonModule} from "@angular/material/button";
+import {MatFormFieldModule} from "@angular/material/form-field";
+import {MatInputModule} from "@angular/material/input";
+import {ReactiveFormsModule} from "@angular/forms";
+import {SharedModule} from "../../shared/shared.module";
 
 
 @NgModule({
@@ -19,7 +24,12 @@ import {EditEquipmentPageComponent} from './pages/edit-equipment-page/edit-equip
     CommonModule,
     AdminEquipmentsRoutingModule,
     AdminRatingsModule,
-    AdminModule
+    AdminModule,
+    MatButtonModule,
+    MatFormFieldModule,
+    MatInputModule,
+    ReactiveFormsModule,
+    SharedModule
   ]
 })
 export class AdminEquipmentsModule {
--- src/app/admin/admin-equipments/pages/edit-equipment-page/edit-equipment-page.component.ts
+++ src/app/admin/admin-equipments/pages/edit-equipment-page/edit-equipment-page.component.ts
@@ -1,16 +1,47 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, Inject, OnInit} from '@angular/core';
+import {BaseEditPage, EditPageState} from "../../../basics/BaseEditPage";
+import {Equipment, EquipmentDto} from "../../../../core/models/equipment";
+import {FormControl, FormGroup, Validators} from "@angular/forms";
+import {RxState} from "@rx-angular/state";
+import {ADMIN_GLOBAL_RX_STATE, AdminGlobalState} from "../../../../core/states/AdminGlobalState";
+import {EquipmentsService} from "../../../../core/services/equipments/equipments.service";
+import {ActivatedRoute} from "@angular/router";
+
+interface EditEquipmentState extends EditPageState<Equipment> {
+
+}
 
 @Component({
   selector: 'app-edit-equipment-page',
   templateUrl: './edit-equipment-page.component.html',
-  styleUrls: ['./edit-equipment-page.component.scss']
+  styleUrls: ['./edit-equipment-page.component.scss'],
+  providers: [RxState]
 })
-export class EditEquipmentPageComponent implements OnInit {
+export class EditEquipmentPageComponent extends BaseEditPage<Equipment, EquipmentDto> implements OnInit {
+  nameFormControl = new FormControl('', [Validators.required]);
 
-  constructor() {
+  constructor(@Inject(ADMIN_GLOBAL_RX_STATE) globalState: RxState<AdminGlobalState>, state: RxState<EditEquipmentState>, service: EquipmentsService, route: ActivatedRoute) {
+    super(globalState, state, service, route);
   }
 
   ngOnInit(): void {
+  }
+
+  protected createDto(): EquipmentDto {
+    return {};
+  }
+
+  protected createFormGroup(): FormGroup {
+    return new FormGroup({
+      nameForm: this.nameFormControl
+    });
+  }
+
+  protected getTitle(): string {
+    return "Equipment bearbeiten";
+  }
+
+  protected updateForms(item: Equipment): void {
   }
 
 }
