42024844001f5c353fe030a0f6b461f3a7133e70
David Huemer <92david.huemer@gmail.com>
2023-03-21 10:40:39 +0100

    Added admin-equipments module

---
+++ src/app/admin/admin-equipments/admin-equipments.module.ts
@@ -0,0 +1,15 @@
+import {NgModule} from '@angular/core';
+import {CommonModule} from '@angular/common';
+
+import {AdminEquipmentsRoutingModule} from './admin-equipments-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    AdminEquipmentsRoutingModule
+  ]
+})
+export class AdminEquipmentsModule {
+}
